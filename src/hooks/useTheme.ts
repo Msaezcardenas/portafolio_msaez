1c019d143a2ac4dcc13080072941160311dc32df
molu <msaezcardenas@gmail.com>
2025-01-27 17:39:35 -0300

    fix

--- src/hooks/useTheme.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { ThemeContext } from '../components/ThemeContext';
-import type { ThemeContextType } from '../components/ThemeContext';
-
-export const useTheme = (): ThemeContextType => {
-  const context = useContext(ThemeContext);
-  if (!context) throw new Error('useTheme must be used within a ThemeProvider');
-  return context;
-};
